e7aa2934be011b1c9d573a8fa2cdffced89c510b
Pavel Gaidukov <mailgpn@gmail.com>
2022-03-23 11:09:43 +0500

    add logging

--- kittybot.py
+++ kittybot.py
@@ -1,12 +1,21 @@
+import logging
 import os
+
 import requests
+
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
-from dotenv import load_dotenv
+
+from dotenv import load_dotenv 
 
 
 load_dotenv()
 secret_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
 URL = 'https://api.thecatapi.com/v1/images/search'
 
 
@@ -14,7 +23,10 @@ def get_new_image():
     try:
         response = requests.get(URL)
     except Exception as error:
-        print(error)
+        # Печатать информацию в консоль теперь не нужно:
+        # всё необходимое будет в логах
+        # print(error)
+        logging.error(f'Ошибка при запросе к основному API: {error}')
         new_url = 'https://api.thedogapi.com/v1/images/search'
         response = requests.get(new_url)
 
