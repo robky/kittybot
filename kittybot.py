41ebd83d341515f52467a9d257c5b87c3ad0f9c6
Pavel Gaidukov <mailgpn@gmail.com>
2022-03-23 09:51:42 +0500

    refactor file -> module

--- kittybot.py
+++ kittybot.py
@@ -1,13 +1,24 @@
+import os
 import requests
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
+from dotenv import load_dotenv
 
-updater = Updater(token='5052093685:AAEL7rHh1-KfwEblWE7gIvTLlJ-9pxDF05M')
+
+load_dotenv()
+secret_token = os.getenv('TOKEN')
 URL = 'https://api.thecatapi.com/v1/images/search'
 
 
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
@@ -20,20 +31,26 @@ def new_cat(update, context):
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    # За счёт параметра resize_keyboard=True сделаем кнопки поменьше
     button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
 
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
         reply_markup=button
     )
 
     context.bot.send_photo(chat.id, get_new_image())
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+def main():
+    updater = Updater(token=secret_token)
 
-updater.start_polling()
-updater.idle()
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ name_demo2.py
@@ -0,0 +1,7 @@
+# kittybot/name_demo2.py
+
+from name_demo import simple_add
+
+
+print(simple_add(5, 3))
+print(__name__)
