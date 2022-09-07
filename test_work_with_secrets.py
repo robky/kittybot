9a317625cdf13b560fa73482ca063ab2fbd3c24b
Pavel Gaidukov <mailgpn@gmail.com>
2022-09-07 11:36:04 +0500

    Delete test_work_with_secrets.py

--- test_work_with_secrets.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-# Теперь переменная TOKEN, описанная в файле .env,
-# доступна в пространстве переменных окружения
-
-token = os.getenv('TOKEN')
-print(token)  # 1896714192:AAFmXXXXPEn6KqewJ13kKKtlnBYqVC_XXXX
