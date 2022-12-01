8785803c39982450829ef5d0cc9ac9b579dc0b09
Pavel <mailgpn@gmail.com>
2022-12-01 17:45:53 +0000

    add dogs

--- kittybot.py
+++ kittybot.py
@@ -6,7 +6,7 @@ import requests
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 
 load_dotenv()
@@ -23,39 +23,48 @@ def get_new_image():
     try:
         response = requests.get(URL)
     except Exception as error:
-        # Печатать информацию в консоль теперь не нужно:
-        # всё необходимое будет в логах
-        # print(error)
         logging.error(f'Ошибка при запросе к основному API: {error}')
         new_url = 'https://api.thedogapi.com/v1/images/search'
         response = requests.get(new_url)
-
-    response = response.json()
-    random_cat = response[0].get('url')
+    try:
+        response = response.json()
+        random_cat = response[0].get('url')
+    except Exception as error:
+        logging.error(f'Ошибка получения url: {error}')
+        random_cat = ''
     return random_cat
 
 
 def new_cat(update, context):
-    chat = update.effective_chat
-    context.bot.send_photo(chat.id, get_new_image())
+    try:
+        chat = update.effective_chat
+        context.bot.send_photo(chat.id, get_new_image())
+    except Exception as error:
+        logging.error(f'Ошибка отправки new_cat: {error}')
 
 
 def wake_up(update, context):
-    chat = update.effective_chat
-    name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+    try:
+        chat = update.effective_chat
+        name = update.message.chat.first_name
+        button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
 
-    context.bot.send_message(
-        chat_id=chat.id,
-        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
-        reply_markup=button
-    )
+        context.bot.send_message(
+            chat_id=chat.id,
+            text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
+            reply_markup=button
+        )
 
-    context.bot.send_photo(chat.id, get_new_image())
+        context.bot.send_photo(chat.id, get_new_image())
+    except Exception as error:
+        logging.error(f'Ошибка отправки wake_up: {error}')
 
 
 def main():
-    updater = Updater(token=secret_token)
+    try:
+        updater = Updater(token=secret_token)
+    except Exception as error:
+        logging.error(f'Ошибка токена: {error}')
 
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
     updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
